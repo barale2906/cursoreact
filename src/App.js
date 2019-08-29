4ec826d3a8e6229b370f181359f72163751eca49
Alexander Barajas <alexanderbarajas@gmail.com>
2019-08-29 18:00:28 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+
+  <>
+  <div className="main-banner img-container" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="caramba" src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Título del banner</p>
+        <p> Subtítulo del banner</p>
+        <a href="modulo.co" className="button">Botón del banner</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
+  <div className="ed-grid m-grid-3">
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      <Curso/>
+      
+  </div>
+    
+
+  </>
+
+)
 
 export default App;
---
+++ src/curso.jsx
@@ -0,0 +1,35 @@
+import React from 'react' 
+
+const Curso = () => (
+
+    <article className="card">
+      <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
+        <img src="https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-07/PremiereDesdeCero.png" alt=""/>
+      </div>
+      <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+        <h3 className="t5 s-mb-2 s-center">
+          Programación orientada a objetos con Go
+        </h3>
+        <div className="s-mb-2 s-main-center card-teachers">
+          <div className="card__teacher s-cross-center">
+            <div className="card__avatar s-mr-1">
+              <div className="circle img-container">
+                <img src="https://api.ed.team/files/avatars/b24584ff-5e99-4aac-bb63-064f3ac50c3e.jpg" alt=""/>
+              </div>
+            </div>
+            <span className="small">Alexys Lozada</span>
+          </div>
+         
+          
+        </div>
+        <div className="s-main-center">
+          <a className="button--ghost-alert button--tiny" href="modulodegestion.co">$ 20USD</a>
+        </div>
+      </div>
+    </article>
+
+)
+
+export default Curso
+
+
