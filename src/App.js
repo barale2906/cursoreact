da66d39ddc5e28ae57523a2b38a6f97748483242
Alexander Barajas <alexanderbarajas@gmail.com>
2019-08-29 23:48:07 -0500

    finalizando componentes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import React from 'react';
 import './styles/styles.scss';
 import Curso from './curso'
 
+const cursos =[
+
+  {
+    "title":"Inicio de iglesia",
+    "imagen":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2019-08/Especialidad%20Angular.png",
+    "price":" 0 USD"
+  },
+  {
+    "title":"Bautismo",
+    "imagen":"https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-06/SEO%20y%20analitica%20web.png",
+    "price":" 10 USD"
+  },
+  {
+    "title":"Diaconado",
+    "imagen":"https://drupal.ed.team/sites/default/files/2018-11/java-especialidad.jpg",
+    "price":" 30 USD"
+  }
+
+]
+
 const App = () => (
 
   <>
@@ -18,13 +38,9 @@ const App = () => (
   </div>
 </div>
   <div className="ed-grid m-grid-3">
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      <Curso/>
-      
+      {
+        cursos.map( curso => <Curso title={curso.title} imagen={curso.imagen} price={curso.price}  />)
+      }    
   </div>
     
 
